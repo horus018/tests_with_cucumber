72790b825318e31bfef38c2aa013f6f67567dcc8
lucasruby <rubira820@gmail.com>
2023-10-11 15:58:20 -0300

    - configurações para rodar testes em outros navegadores

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,27 +16,47 @@ require_relative 'page_helper.rb'
 # puts ENV['VARIAVEL_TESTE']
 # vai imprimir 'eita nois', MUITO TESÃO ISSO!!!
 
+BROWSER = ENV['BROWSER']
 AMBIENTE = ENV['AMBIENTE']
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
 
+# torna a classe PageObjects global para ser acessada em qualquer lugar
 World(PageObjects)
+
+Capybara.register_driver :selenium do |app|
+
+  if BROWSER.eql?('chrome')
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  elsif BROWSER.eql?('chrome_headless')
+    chrome_options = Selenium::WebDriver::Chrome::Options.new
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  elsif BROWSER.eql?('firefox')
+    # Configurar o caminho para o executável do Firefox, sem isso não ta funcionando
+    browser_options = Selenium::WebDriver::Firefox::Options.new(binary: '/home/lucas/Downloads/firefox/firefox')
+    Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options)
+  elsif BROWSER.eql?('firefox_headless')
+    browser_options = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'], binary: '/home/lucas/Downloads/firefox/firefox')
+    Capybara::Selenium::Driver.new(app, browser: :firefox, options: browser_options)
+  elsif BROWSER.eql?('safari')
+    Capybara::Selenium::Driver.new(app, :browser => :safari)
+  elsif BROWSER.eql?('edge')
+    Capybara::Selenium::Driver.new(app, :browser => :edge)
+  end
+
+end
 
 Capybara.configure do |config|
 
-  Capybara.register_driver :selenium do |app|
-    # Configurar o caminho para o executável do Firefox
-    options = Selenium::WebDriver::Firefox::Options.new(binary: '/home/lucas/Downloads/firefox/firefox')
-    Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
-  end
-
-  #selenium (firefox) selenium_chrome (chrome) #selenium_chrome_headless (cli)
+  # selenium (firefox) selenium_chrome (chrome) #selenium_chrome_headless (cli)
   # o primeiro abre no chrome o segundo cli
   config.default_driver = :selenium
 
-  #url do site em q os testes serao executados
+  # url do site em q os testes serao executados
   config.app_host = CONFIG['url_padrao']
 
-  #tempo maximo em segundos de espera para encontrar um elemento na tela, se nao encontrar ele da erro
+  # tempo maximo em segundos de espera para encontrar um elemento na tela, se nao encontrar ele da erro
   config.default_max_wait_time = 10
 
 end
